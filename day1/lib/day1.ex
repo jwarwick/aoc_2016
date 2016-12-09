861477493f7855f154d01991da4c5b8ddac6de7d
John Warwick <jwarwick@gmail.com>
2016-12-09 17:18:08 -0500

    Solved Day 1, Part 1

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -1,18 +1,51 @@
 defmodule Day1 do
   @moduledoc """
-  Documentation for Day1.
+  Compute the Manhattan distance between two points given a path.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day1.hello
-      :world
-
+  Compute manhattan distance of steps specificed in a file"
   """
-  def hello do
-    :world
+  def distance_file(file_path), do: distance(File.read!(file_path))
+
+  @doc """
+  Given a string of steps, compute the manhattan distance between start and end
+  """
+  def distance(path) do
+    start_point = {0, 0}
+    steps = parse_path(path)
+    {_heading, end_point} = Enum.reduce(steps, {:north, start_point}, &take_step/2)
+    manhattan_distance(start_point, end_point)
   end
+
+  @doc """
+  Compute the manhattan distance between two points
+  """
+  def manhattan_distance({x1, y1}, {x2, y2}) do
+    abs(x1 - x2) + abs(y1 - y2)
+  end
+
+  defp parse_path(str) do
+    str
+    |> String.split(",")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&convert_step/1)
+  end
+
+  defp convert_step(<<?R, size :: binary>>) do
+    {:right, String.to_integer(size)}
+  end
+  defp convert_step(<<?L, size :: binary>>) do
+    {:left, String.to_integer(size)}
+  end
+
+  defp take_step({:right, size}, {:north, {x, y}}), do: {:east, {x + size, y}}
+  defp take_step({:right, size}, {:east, {x, y}}), do: {:south, {x, y - size}}
+  defp take_step({:right, size}, {:south, {x, y}}), do: {:west, {x - size, y}}
+  defp take_step({:right, size}, {:west, {x, y}}), do: {:north, {x, y + size}}
+  defp take_step({:left, size}, {:north, {x, y}}), do: {:west, {x - size, y}}
+  defp take_step({:left, size}, {:west, {x, y}}), do: {:south, {x, y - size}}
+  defp take_step({:left, size}, {:south, {x, y}}), do: {:east, {x + size, y}}
+  defp take_step({:left, size}, {:east, {x, y}}), do: {:north, {x, y + size}}
+
 end
--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -2,7 +2,25 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "two steps" do
+    assert 5 == Day1.distance("R2, L3")
   end
+
+  test "three steps" do
+    assert 2 == Day1.distance("R2, R2, R2")
+  end
+
+  test "four steps" do
+    assert 12 == Day1.distance("R5, L5, R5, R3")
+  end
+
+  test "manhattan math" do
+    assert 0 == Day1.manhattan_distance({0, 0}, {0, 0})
+    assert 1 == Day1.manhattan_distance({1, 1}, {2, 1})
+    assert 1 == Day1.manhattan_distance({1, 1}, {1, 2})
+    assert 1 == Day1.manhattan_distance({2, 1}, {1, 1})
+    assert 1 == Day1.manhattan_distance({1, 2}, {1, 1})
+    assert 5 == Day1.manhattan_distance({1, 2}, {3, -1})
+  end
+
 end
