ae4f3f854bac232458a047d68d7a028470c42c30
John Warwick <jwarwick@gmail.com>
2016-12-13 20:59:09 +0100

    Completed Day 2, Part 2

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -5,32 +5,8 @@ defmodule Day2 do
   Given a string of commands, figure out the number to enter
   """
 
-  @doc """
-  Decode a file
-  """
-  def decode_file(path) do
-    path
-    |> File.read!
-    |> decode
-  end
-
-  @doc """
-  Given a string of up, down, left, right commands, return the number (as a string to return)
-  A digit is generated for each line of text. Assume we start at '5' in the middle of the keypad.
-  """
-  def decode(str) do
-    str
-    |> String.trim_trailing
-    |> String.split("\n")
-    |> Enum.reduce(%{idx: 5, result: []}, &decode_entry/2)
-    |> extract_result
-    |> Enum.reverse
-    |> Enum.map(&Integer.to_string/1)
-    |> Enum.join
-  end
-
-  # The lookup table for the keypad
-  @keypad %{
+  # The lookup table for the keypad in part 1
+  @keypad1 %{
     1 => %{?U => 1, ?L => 1, ?D => 4, ?R => 2},
     2 => %{?U => 2, ?L => 1, ?D => 5, ?R => 3},
     3 => %{?U => 3, ?L => 2, ?D => 6, ?R => 3},
@@ -44,20 +20,77 @@ defmodule Day2 do
     9 => %{?U => 6, ?L => 8, ?D => 9, ?R => 9}
   }
 
+  # The lookup table for the keypad in part 2
+  @keypad2 %{
+    1 => %{?U => 1, ?L => 1, ?D => 3, ?R => 1},
+    2 => %{?U => 2, ?L => 2, ?D => 6, ?R => 3},
+    3 => %{?U => 1, ?L => 2, ?D => 7, ?R => 4},
+
+    4 => %{?U => 4, ?L => 3, ?D => 8, ?R => 4}, 
+    5 => %{?U => 5, ?L => 5, ?D => 5, ?R => 6},
+    6 => %{?U => 2, ?L => 5, ?D => ?A, ?R => 7},
+
+    7 => %{?U => 3, ?L => 6, ?D => ?B, ?R => 8},
+    8 => %{?U => 4, ?L => 7, ?D => ?C, ?R => 9},
+    9 => %{?U => 9, ?L => 8, ?D => 9, ?R => 9},
+
+    ?A => %{?U => 6, ?L => ?A, ?D => ?A, ?R => ?B},
+    ?B => %{?U => 7, ?L => ?A, ?D => ?D, ?R => ?C},
+    ?C => %{?U => 8, ?L => ?B, ?D => ?C, ?R => ?C},
+    ?D => %{?U => ?B, ?L => ?D, ?D => ?D, ?R => ?D}
+  }
+
+  @doc """
+  Decode a file
+  """
+  def decode_file_part1(path) do
+    path
+    |> File.read!
+    |> decode_part_1
+  end
+
+  def decode_file_part2(path) do
+    path
+    |> File.read!
+    |> decode_part_2
+  end
+
+  @doc """
+  Given a string of up, down, left, right commands, return the number (as a string to return)
+  A digit is generated for each line of text. Assume we start at '5' in the middle of the keypad.
+  """
+  def decode_part_1(str), do: decode(str, @keypad1)
+
+  def decode_part_2(str), do: decode(str, @keypad2)
+
+  defp decode(str, keypad) do
+    str
+    |> String.trim_trailing
+    |> String.split("\n")
+    |> Enum.reduce(%{idx: 5, result: [], keypad: keypad}, &decode_entry/2)
+    |> extract_result
+    |> Enum.reverse
+    |> Enum.map(&convert_to_string/1)
+    |> Enum.join
+  end
+
   defp extract_result(%{result: result}), do: result
 
-  defp decode_entry(entry, %{idx: idx, result: result}) do
-    next_idx = _decode(idx, entry)
-    %{idx: next_idx, result: [next_idx | result]}
+  defp convert_to_string(x) when x <= 9, do: Integer.to_string(x)
+  defp convert_to_string(x), do: <<x>>
+
+  defp decode_entry(entry, %{idx: idx, result: result, keypad: keypad}) do
+    next_idx = _decode(idx, keypad, entry)
+    %{idx: next_idx, result: [next_idx | result], keypad: keypad}
   end
 
-  defp _decode(idx, <<>>), do: idx
-  defp _decode(idx, <<dir, rest::binary>>) do
-    next_step(idx, dir)
-    |> _decode(rest)
+  defp _decode(idx, _keypad, <<>>), do: idx
+  defp _decode(idx, keypad, <<dir, rest::binary>>) do
+    next_step(idx, dir, keypad)
+    |> _decode(keypad, rest)
   end
 
-  defp next_step(idx, dir) do
-    @keypad[idx][dir]
+  defp next_step(idx, dir, keypad) do
+    keypad[idx][dir]
   end
 end
--- day2/test/day2_test.exs
+++ day2/test/day2_test.exs
@@ -3,19 +3,19 @@ defmodule Day2Test do
   doctest Day2
 
   test "empty string" do
-    assert "5" == Day2.decode("")
+    assert "5" == Day2.decode_part_1("")
   end
 
   test "one step" do
-    assert "2" == Day2.decode("U")
+    assert "2" == Day2.decode_part_1("U")
   end
 
   test "stop at edge" do
-    assert "4" == Day2.decode("LL")
+    assert "4" == Day2.decode_part_1("LL")
   end
 
   test "longer string" do
-    assert "9" == Day2.decode("UULLDDDDDDDDDDRRUDDDRRR")
+    assert "9" == Day2.decode_part_1("UULLDDDDDDDDDDRRUDDDRRR")
   end
 
   test "two lines" do
@@ -23,7 +23,7 @@ defmodule Day2Test do
     UL
     DRDR
     """
-    assert "19" == Day2.decode(str)
+    assert "19" == Day2.decode_part_1(str)
   end
 
   test "sample input" do
@@ -33,6 +33,20 @@ defmodule Day2Test do
     LURDL
     UUUUD
     """
-    assert "1985" == Day2.decode(str)
+    assert "1985" == Day2.decode_part_1(str)
+  end
+
+  test "part 2 1 step" do
+    assert "5" == Day2.decode_part_2("U")
+  end
+
+  test "sample input, part 2" do
+    str = """
+    ULL
+    RRDDD
+    LURDL
+    UUUUD
+    """
+    assert "5DB3" == Day2.decode_part_2(str)
   end
 end
