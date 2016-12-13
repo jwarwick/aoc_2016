0b0c7fc9bce2c0a1c496e5eeb95560ab20cb6394
John Warwick <jwarwick@gmail.com>
2016-12-13 21:17:51 +0100

    Completed Day 3, Part 2

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -12,19 +12,51 @@ defmodule Day3 do
     |> valid_count
   end
 
+  def valid_transpose_count_file(path) do
+    path
+    |> File.read!
+    |> valid_transpose_count
+  end
+
   @doc """
   Split the given string into possible triangles. Return the number of valid triangles.
   Assumes one triangle per line.
   """
   def valid_count(str) do
     str
+    |> clean_data
+    |> test_data
+  end
+
+  @doc """
+  Split the given string into possible triangles. Return the number of valid triangles.
+  Assumes triangles are specified in columns
+  """
+  def valid_transpose_count(str) do
+    str
+    |> clean_data
+    |> transpose_columns([])
+    |> test_data
+  end
+
+  defp clean_data(str) do
+    str
     |> String.trim_trailing
     |> String.split("\n")
     |> Enum.map(&convert_to_ints/1)
+  end
+
+  defp test_data(arr) do
+    arr
     |> Enum.map(&build_permutations/1)
     |> Enum.map(&compute_valid/1)
     |> Enum.map(&Enum.all?/1)
     |> Enum.count(&(&1 == true))
+  end
+
+  defp transpose_columns([], result), do: result
+  defp transpose_columns([{a1, b1, c1}, {a2, b2, c2}, {a3, b3, c3} | rest], result) do
+    transpose_columns(rest, [{a1, a2, a3}, {b1, b2, b3}, {c1, c2, c3} | result])
   end
 
   defp convert_to_ints(str) do
--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -17,4 +17,16 @@ defmodule Day3Test do
     """
     assert 1 == Day3.valid_count(str)
   end
+
+  test "transpose columns" do
+    str = """
+    3 3 3
+    4 4 4
+    5 5 5
+    5 5 5
+    10 10 10
+    25 25 25
+    """
+    assert 3 == Day3.valid_transpose_count(str)
+  end
 end
