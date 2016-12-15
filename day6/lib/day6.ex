166da20bd99fe5aaaa5092e049448cabfe05037b
John Warwick <jwarwick@gmail.com>
2016-12-15 20:59:45 +0100

    Day 6, completed Part 1

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -1,18 +1,45 @@
 defmodule Day6 do
   @moduledoc """
-  Documentation for Day6.
+  Decode repitition code of a list of strings
   """
+
+  def decode_from_file(path) do
+    path
+    |> File.read!
+    |> String.split("\n")
+    |> decode
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day6.hello
-      :world
-
+  Given a list of strings, compute the string built from the character more frequently
+  in each position.
   """
-  def hello do
-    :world
+  def decode(list) do
+    initial = List.duplicate(%{}, String.length(hd(list)))
+    list
+    |> Enum.reduce(initial, &count_string/2)
+    |> Enum.map(&map_max/1)
+    |> IO.iodata_to_binary
+  end
+
+  defp count_string(string, acc) do
+    string
+    |> to_charlist
+    |> Enum.with_index
+    |> Enum.reduce(acc, &count_char/2)
+  end
+
+  defp count_char({value, index}, acc) do
+    curr_map = Enum.at(acc, index)
+    updated_map = Map.update(curr_map, value, 1, &(&1 + 1))
+    List.replace_at(acc, index, updated_map)
+  end
+
+  defp map_max(map) do
+    map
+    |> Map.to_list
+    |> Enum.sort(fn ({_k1, v1}, {_k2, v2}) -> v1 >= v2 end)
+    |> Enum.map(fn {key, _value} -> key end)
+    |> hd
   end
 end
--- day6/test/day6_test.exs
+++ day6/test/day6_test.exs
@@ -2,7 +2,33 @@ defmodule Day6Test do
   use ExUnit.Case
   doctest Day6
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "one entry" do
+    assert "eedadn" == Day6.decode(["eedadn"])
+  end
+
+  test "three entries" do
+    assert "eedadn" == Day6.decode(["eedadn", "bbbbbb", "eedadn"])
+  end
+
+  test "example" do
+    list = ~w(
+    eedadn
+    drvtee
+    eandsr
+    raavrd
+    atevrs
+    tsrnev
+    sdttsa
+    rasrtv
+    nssdts
+    ntnada
+    svetve
+    tesnvt
+    vntsnd
+    vrdear
+    dvrsen
+    enarar
+    )
+    assert "easter" == Day6.decode(list)
   end
 end
