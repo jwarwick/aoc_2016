ec328a394bb9e38db5788e5b20adaf7f89ceae45
John Warwick <jwarwick@gmail.com>
2016-12-21 10:45:30 -0500

    Day 16, initial commit

---
+++ day16/lib/day16.ex
@@ -0,0 +1,18 @@
+defmodule Day16 do
+  @moduledoc """
+  Documentation for Day16.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day16.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
