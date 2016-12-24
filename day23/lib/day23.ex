a0146a3abdca40ceaeb7f1d4527d6259fe6af464
John Warwick <jwarwick@gmail.com>
2016-12-23 22:25:56 -0500

    Day 23, initial commit

---
+++ day23/lib/day23.ex
@@ -0,0 +1,18 @@
+defmodule Day23 do
+  @moduledoc """
+  Documentation for Day23.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day23.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ day23/test/day23_test.exs
@@ -0,0 +1,8 @@
+defmodule Day23Test do
+  use ExUnit.Case
+  doctest Day23
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
