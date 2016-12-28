48df1a502ba8bc53e11227dafab37062a28a9a7b
John Warwick <jwarwick@gmail.com>
2016-12-28 10:57:00 -0500

    Day 25, inital commit

---
+++ day25/lib/day25.ex
@@ -0,0 +1,18 @@
+defmodule Day25 do
+  @moduledoc """
+  Documentation for Day25.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day25.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
