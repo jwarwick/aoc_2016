58d01e38b912886d6cbfc679d1b93a4c562440c4
John Warwick <jwarwick@gmail.com>
2019-02-07 22:06:02 -0500

    Day 19, part 2

--- day19/day19.go
+++ day19/day19.go
@@ -3,47 +3,69 @@ package main
 import (
 	"container/list"
 	"fmt"
+	"math"
 )
 
 func main() {
 	input := 3004953
 
-	part1 := bestPosition(input)
+	part1 := bestPosition(input, play)
 	fmt.Println("Part 1: ", part1)
+
+	part2 := bestPosition(input, playAcross)
+	fmt.Println("Part 2: ", part2)
 }
 
-type elf struct {
-	num      int
-	presents int
+func bestPosition(count int, fp func(*list.List) int) int {
+	queue := buildQueue(count)
+	return fp(queue)
 }
 
-func bestPosition(count int) int {
+func buildQueue(count int) *list.List {
 	queue := list.New()
-
 	for i := 1; i <= count; i++ {
-		queue.PushBack(elf{i, 1})
+		queue.PushBack(i)
 	}
-	return play(queue)
+	return queue
 }
 
 func play(q *list.List) int {
 	curr := q.Front()
 	for q.Len() > 1 {
-		left := curr.Next()
-		if left == nil {
-			left = q.Front()
-		}
-		currElf := curr.Value.(elf)
-		leftElf := left.Value.(elf)
-		currElf.presents += leftElf.presents
-		next := left.Next()
-		if next == nil {
-			next = q.Front()
-		}
-		curr = next
+		left := nextElf(curr, q)
+		curr = nextElf(left, q)
 		q.Remove(left)
 	}
 	last := q.Front()
-	lastElf := last.Value.(elf)
-	return lastElf.num
+	return last.Value.(int)
+}
+
+func playAcross(q *list.List) int {
+	curr := q.Front()
+	dist := int(math.Floor(float64(q.Len() / 2)))
+	left := curr
+	for i := 0; i < dist; i++ {
+		left = nextElf(left, q)
+	}
+
+	for q.Len() > 1 {
+		next := nextElf(left, q)
+		q.Remove(left)
+		if q.Len()%2 == 0 {
+			next = nextElf(next, q)
+		}
+		left = next
+
+		curr = nextElf(curr, q)
+	}
+	last := q.Front()
+	return last.Value.(int)
+}
+
+func nextElf(e *list.Element, q *list.List) *list.Element {
+	n := e.Next()
+	if n == nil {
+		n = q.Front()
+	}
+	return n
 }
--- day19/day19_test.go
+++ day19/day19_test.go
@@ -7,7 +7,16 @@ import (
 func TestGame(t *testing.T) {
 	expected := 3
 	count := 5
-	result := bestPosition(count)
+	result := bestPosition(count, play)
+	if result != expected {
+		t.Errorf("Input: %d, expected: %d, got: %d", count, expected, result)
+	}
+}
+
+func TestAcrossGame(t *testing.T) {
+	expected := 2
+	count := 5
+	result := bestPosition(count, playAcross)
 	if result != expected {
 		t.Errorf("Input: %d, expected: %d, got: %d", count, expected, result)
 	}
